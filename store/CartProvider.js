31b1276685dd2a5b571ad30273eb1dc317383ac1
Mike Coon <mac718@gmail.com>
2021-12-07 18:43:43 -0800

    update cartReducer to increase quantity rather than add a new item when the added item alredy exists in the items array

--- store/CartProvider.js
+++ store/CartProvider.js
@@ -8,8 +8,24 @@ const defaultCartState = {
 
 const cartReducer = (state, action) => {
   if (action.type === "ADD") {
-    let updatedItems = state.items.concat(action.item);
-    return { items: updatedItems, total: 0 };
+    let sameItemIndex = state.items.findIndex(
+      (item) => item.id === action.item.id
+    );
+    if (sameItemIndex === -1) {
+      console.log("hello");
+      let updatedItems = state.items.concat(action.item);
+      return { items: updatedItems, total: 0 };
+    } else {
+      let updatedQuantity =
+        state.items[sameItemIndex].quantity + Number(action.item.quantity);
+      let copyStateItems = state.items.slice(0);
+
+      copyStateItems[sameItemIndex] = {
+        ...state.items[sameItemIndex],
+        quantity: updatedQuantity,
+      };
+      return { items: copyStateItems, total: 0 };
+    }
   }
   return defaultCartState;
 };
@@ -19,6 +35,7 @@ const CartProvider = (props) => {
     cartReducer,
     defaultCartState
   );
+  console.log(cartState);
   const addItemToCartHandler = (event, item) => {
     event.preventDefault();
     dispatchCartAction({ type: "ADD", item });
