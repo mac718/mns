41380a35632b27ea0d9a97e4419146b262b23ce5
Mike Coon <mac718@gmail.com>
2021-12-11 10:58:09 -0800

    add animation to cart button when items are added

--- components/CartButton.js
+++ components/CartButton.js
@@ -1,15 +1,36 @@
 import styles from "./CartButton.module.css";
 import { MdShoppingCart } from "react-icons/md";
-import { useContext } from "react";
+import { useContext, useState, useEffect } from "react";
 import CartContext from "../store/cart-context";
 
 const CartButton = (props) => {
+  const [btnAnimated, setBtnAnimated] = useState(false);
   const cartCtx = useContext(CartContext);
-  let numberOfItems = cartCtx.items.reduce((total, item) => {
+
+  const buttonClasses = `${styles.cart} ${btnAnimated ? styles.bump : ""}`;
+
+  const { items } = cartCtx;
+
+  let numberOfItems = items.reduce((total, item) => {
     return Number(total) + Number(item.quantity);
   }, 0);
+
+  useEffect(() => {
+    if (items.length === 0) {
+      return;
+    }
+    setBtnAnimated(true);
+
+    const timer = setTimeout(() => {
+      setBtnAnimated(false);
+    }, 300);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [items]);
   return (
-    <div className={styles.cart} onClick={props.onClick}>
+    <div className={buttonClasses} onClick={props.onClick}>
       <MdShoppingCart />
       <span className={styles.badge}>{numberOfItems}</span>
     </div>
