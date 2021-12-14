16939c94baac05b37947db297677c75f2faf4f25
Mike Coon <mac718@gmail.com>
2021-12-13 17:54:50 -0800

    rename units varable in CheckoutButtons lineItems anad reconfigure the objects to accurately create order line items; make necessary changes to purchase units array

--- components/Checkout/CheckoutButtons.js
+++ components/Checkout/CheckoutButtons.js
@@ -7,6 +7,7 @@ const CheckoutButtons = (props) => {
   const [paid, setPaid] = React.useState(false);
   const [error, setError] = React.useState(null);
   const [items, setItems] = React.useState([]);
+  //const [total, setTotal] = React.useState(0);
   const [shipping, setShipping] = React.useState([]);
   console.log(props.zip);
   const paypalRef = React.useRef();
@@ -14,39 +15,61 @@ const CheckoutButtons = (props) => {
     setShipping(props.shippingService);
   }, [props.shippingService]);
   React.useEffect(() => {
-    let units;
+    let lineItems;
+    let total;
     if (localStorage.getItem("items")) {
       setItems(JSON.parse(localStorage.getItem("items")));
-      units = JSON.parse(localStorage.getItem("items")).map((item) => {
+      lineItems = JSON.parse(localStorage.getItem("items")).map((item) => {
         return {
+          name: `${item.name} ${item.type}`,
+          unit_amount: { value: String(item.price), currency_code: "USD" },
+          quantity: item.quantity,
           description: `${item.name} ${item.type}`,
-          amount: { value: item.price * item.quantity },
-          reference_id: item.id,
         };
       });
+
+      if (localStorage.total) {
+        console.log(localStorage.getItem("total"));
+        total = localStorage.getItem("total");
+      }
     }
 
     //setShipping(localStorage.getItem("shipping").split(","));
 
-    console.log("shipping", props.shippingService);
+    console.log("shipping", Number(total) + Number(props.shippingService[1]));
 
-    units = [
-      ...units,
+    lineItems = [
+      ...lineItems,
       {
-        description: `Shipping: ${props.shippingService[0]}`,
-        amount: { value: Number(props.shippingService[1]) },
-        reference_id: "shipping",
+        name: `Shipping: ${props.shippingService[0]}`,
+        unit_amount: { value: props.shippingService[1], currency_code: "USD" },
+        quantity: 1,
+        description: "Shipping",
       },
     ];
 
-    console.log(units);
+    console.log(lineItems);
     window.paypal
       .Buttons({
         createOrder: (data, actions) => {
           console.log("data", data);
           return actions.order.create({
             intent: "CAPTURE",
-            purchase_units: units,
+            purchase_units: [
+              {
+                amount: {
+                  value: Number(total) + Number(props.shippingService[1]),
+                  breakdown: {
+                    item_total: {
+                      currency_code: "USD",
+                      value: Number(total) + Number(props.shippingService[1]),
+                    },
+                  },
+                },
+                items: lineItems,
+                description: "single order",
+              },
+            ],
           });
         },
         onApprove: async (data, actions) => {
@@ -58,14 +81,17 @@ const CheckoutButtons = (props) => {
           setError(err), console.error(err);
         },
         onShippingChange: (data, actions) => {
-          if (data.shipping_address.zip_code !== enteredZip) {
-            actions.reject();
-          }
+          console.log(data.shipping_address);
+          // if (data.shipping_address.zip_code !== enteredZip) {
+          //   actions.reject("Entered zip and Address zip don't match.");
+          // }
           if (
-            data.shipping_address.country_code !== "US" ||
-            data.shipping_address.country_code !== "CAN"
+            !(
+              data.shipping_address.country_code === "US" ||
+              data.shipping_address.country_code === "CA"
+            )
           ) {
-            actions.reject();
+            actions.reject("wrong country");
           }
           actions.resolve();
         },
