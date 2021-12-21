39f1ef120c82021f233d9189933750961ecce3a7
Mike Coon <mac718@gmail.com>
2021-12-21 11:16:34 -0800

    add console.log to jars api route

--- pages/api/jars.js
+++ pages/api/jars.js
@@ -2,7 +2,9 @@ import connectDB from "../../middleware/mongodb";
 import Jar from "../../models/jar";
 
 const handler = async (req, res) => {
+  console.log("HELLLOEOOOEOE");
   const jars = await Jar.find();
+  console.log(jars);
   res.json({ jars });
 };
 
