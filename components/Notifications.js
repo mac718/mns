de21334810314cb7667a1e9d2d94bc7675ec156d
Mike Coon <mac718@gmail.com>
2023-02-20 10:37:25 -0800

    update Notifications

--- components/Notifications.js
+++ components/Notifications.js
@@ -3,9 +3,8 @@ import styles from "./Notifications.module.css";
 const Notifications = () => {
   return (
     <aside className={styles.notifications}>
-      **I&apos;m currently processing orders from 2/15-2/16. Also, I&apos;m
-      selling my Rooney Heritage Stubby 1xl brush for $200. Email me with any
-      questions! Thanks!**
+      **I&apos;m currently processing orders from 2/17-2/20. No shipments today
+      - Presidents Day. Thanks!**
     </aside>
   );
 };
