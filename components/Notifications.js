0f49487f2b4aa66886470d0bd243c0f432fb5bad
Mike Coon <mac718@gmail.com>
2023-09-15 10:04:11 -0700

    update Notifications

--- components/Notifications.js
+++ components/Notifications.js
@@ -3,7 +3,7 @@ import styles from "./Notifications.module.css";
 const Notifications = () => {
   return (
     <aside className={styles.notifications}>
-      **I&apos;m currently processing orders from 9/13-9/14. Thanks!**
+      **I&apos;m currently processing orders from 9/14-9/15. Thanks!**
     </aside>
   );
 };
