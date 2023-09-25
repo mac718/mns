a45109b5daef24de8c4aa60df365287db5eafa9f
Mike Coon <mac718@gmail.com>
2023-09-25 07:42:08 -0700

    center dropdown in dash page

--- pages/administration/dash.js
+++ pages/administration/dash.js
@@ -1,11 +1,17 @@
 import connectDB from "../../middleware/mongodb";
 import Product from "../../models/product";
+import styles from "./Dash.module.css";
+import { getSession } from "next-auth/react";
 
 const Dash = ({ products }) => {
   const options = products.map((product) => (
     <option>{`${product.scent} ${product.type}`}</option>
   ));
-  return <select>{options}</select>;
+  return (
+    <main className={styles.main}>
+      <select>{options}</select>
+    </main>
+  );
 };
 
 export default Dash;
@@ -17,6 +23,17 @@ export async function getServerSideProps(context) {
   });
 
   const products = await fetchProducts();
+
+  const { req } = context;
+  const session = await getSession({ req });
+
+  console.log("session", session);
+
+  if (!session || !session.user._doc.isAdmin) {
+    return {
+      redirect: { destination: "/auth/signin" },
+    };
+  }
 
   return {
     props: { products: JSON.parse(JSON.stringify(products)) },
