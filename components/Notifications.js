9cdf85026aaae4a2b2d38f3f1b30475a2fc088a8
Mike Coon <mac718@gmail.com>
2023-11-30 19:32:55 -0800

    update Notifications

--- components/Notifications.js
+++ components/Notifications.js
@@ -3,7 +3,7 @@ import styles from "./Notifications.module.css";
 const Notifications = () => {
   return (
     <aside className={styles.notifications}>
-      **I&apos;m currently processing orders from 11/26-11/27. 20% off for Cyber
+      **I&apos;m currently processing orders from 11/29-11/30. 20% off for Cyber
       Monday! Thanks!**
     </aside>
   );
