390cb5067cfa9f1dea6d4ff7ee6ca298447dfd27
Mike Coon <mac718@gmail.com>
2024-12-18 06:30:25 -0800

    update Notifications

--- components/Notifications.js
+++ components/Notifications.js
@@ -3,7 +3,7 @@ import styles from "./Notifications.module.css";
 const Notifications = () => {
   return (
     <aside className={styles.notifications}>
-      **I&apos;m currently processing orders from 12/12-12/16. The USPS shipping
+      **I&apos;m currently processing orders from 12/16-12/18. The USPS shipping
       deadline for a pre-Christmas delivery is 12/18 - Get your Christmas orders
       in by then! Thanks!**
     </aside>
