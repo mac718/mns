30d057ef4a52a47888bde50f340f48e545ad1203
Mike Coon <mac718@gmail.com>
2025-01-20 07:52:13 -0800

    update Notifications

--- components/Notifications.js
+++ components/Notifications.js
@@ -3,7 +3,8 @@ import styles from "./Notifications.module.css";
 const Notifications = () => {
   return (
     <aside className={styles.notifications}>
-      **I&apos;m currently processing orders from 1/15-1/16. Thanks!!**
+      **I&apos;m currently processing orders from 1/17-1/20. No shipments today
+      (1/20) - USPS is closed in observance of MLK Day. Thanks!!**
     </aside>
   );
 };
