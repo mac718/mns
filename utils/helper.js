e1523c3e4e94a8868b9226ce92681a5109b17786
Mike Coon <mac718@gmail.com>
2025-01-21 15:22:07 -0800

    Created utils folder in root with helper.js file; created orderProductList function in helper.js

---
+++ utils/helper.js
@@ -0,0 +1,39 @@
+import connectDB from "../middleware/mongodb";
+import Product from "../models/product";
+
+export const orderProductList = async (type) => {
+  const fetchInStockProducts = connectDB(async () => {
+    const products = await Product.find({ type: type, inStock: { $gte: 1 } });
+    return products;
+  });
+
+  const fetchOutOfStockProducts = connectDB(async () => {
+    const products = await Product.find({ type: type, inStock: { $lte: 0 } });
+    return products;
+  });
+
+  const inStockProducts = await fetchInStockProducts();
+  const outOfStockProducts = await fetchOutOfStockProducts();
+
+  inStockProducts.sort((a, b) => {
+    if (a.scent < b.scent) {
+      return -1;
+    } else if (a.scent > b.scent) {
+      return 1;
+    } else {
+      return 0;
+    }
+  });
+
+  outOfStockProducts.sort((a, b) => {
+    if (a.scent < b.scent) {
+      return -1;
+    } else if (a.scent > b.scent) {
+      return 1;
+    } else {
+      return 0;
+    }
+  });
+
+  return [inStockProducts, outOfStockProducts];
+};
