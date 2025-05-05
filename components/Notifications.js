52a865ff378e1335af392912af0cf9bbb36669eb
Mike Coon <mac718@gmail.com>
2025-05-05 10:38:47 -0700

    udpated notifications

--- components/Notifications.js
+++ components/Notifications.js
@@ -3,7 +3,7 @@ import styles from "./Notifications.module.css";
 const Notifications = () => {
   return (
     <aside className={styles.notifications}>
-      **I&apos;m currently processing orders from 4/4. I just corrected a bug on
+      **I&apos;m currently processing orders from 5/1. I just corrected a bug on
       the shaving sticks page causing it to list available pucks instead of
       sticks. Thanks!!**
     </aside>
