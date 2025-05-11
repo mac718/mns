1a7b85a4ce6564f929021c6416a99d095865d9d7
Mike Coon <mac718@gmail.com>
2025-05-10 18:06:25 -0700

    notice update

--- components/Notifications.js
+++ components/Notifications.js
@@ -3,9 +3,7 @@ import styles from "./Notifications.module.css";
 const Notifications = () => {
   return (
     <aside className={styles.notifications}>
-      **I&apos;m currently processing orders from 5/1. I just corrected a bug on
-      the shaving sticks page causing it to list available pucks instead of
-      sticks. Thanks!!**
+      **I&apos;m currently processing orders from 5/9. Shave sticks are back! **
     </aside>
   );
 };
