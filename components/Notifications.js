b4e6f7518b82889991be45f575f17d6dc156e755
Mike Coon <mac718@gmail.com>
2025-07-19 12:53:11 -0700

    posted moving notification

--- components/Notifications.js
+++ components/Notifications.js
@@ -3,7 +3,11 @@ import styles from "./Notifications.module.css";
 const Notifications = () => {
   return (
     <aside className={styles.notifications}>
-      **I&apos;m currently processing orders from 7/1. Shave sticks are back! **
+      **I&apos;m currently processing orders from 7/18. We are preparing for a
+      big move to NJ on August 1. Shipments will likely cease for at least two
+      weeks after that while we wait for out furniture and cars to arrive. I
+      will get things up and running and soon as possible in the new location.
+      Thanks! **
     </aside>
   );
 };
