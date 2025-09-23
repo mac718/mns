c08c22667759a369e418e1430dccc49399eed523
Mike Coon <mac718@gmail.com>
2025-09-23 12:42:02 -0400

    updated Notifications

--- components/Notifications.js
+++ components/Notifications.js
@@ -3,9 +3,7 @@ import styles from "./Notifications.module.css";
 const Notifications = () => {
   return (
     <aside className={styles.notifications}>
-      **I&apos;m currently processing orders from 9/18. I&apos;m currently not
-      able to send or receive emails from mike@mikesnaturalsoaps.com. Please use
-      mikeatmikesnaturalsoaps@gmail.com instead. Thanks! **
+      **I&apos;m currently processing orders from 9/22. Thanks! **
     </aside>
   );
 };
