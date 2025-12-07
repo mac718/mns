b26896c925e1d7b8893254104f603013bfea273b
Mike Coon <mac718@gmail.com>
2025-12-07 09:37:57 -0500

    update bath_soaps page

--- pages/bath_soaps.js
+++ pages/bath_soaps.js
@@ -1,18 +1,75 @@
-import { shavingSoapFormats } from "../shavingProducts";
-import ShavingSoapFormatList from "../components/ShavingSoapFormatList";
-import ShavingSoapFormatListItem from "../components/ShavingSoapFormatListItem";
 import Heading from "../components/Heading";
-import SubHeading from "../components/SubHeading";
+import MainImage from "../components/MainImage";
+import Notifications from "../components/Notifications";
+import ShavingProductsList from "../components/ShavingProductsList";
+import ShavingProductItem from "../components/ShavingProductItem";
+import Footer from "../components/Footer";
+import Image from "next/image";
+import { orderProductList } from "../utils/helper";
+import styles from "./shaving_products.module.css";
+import shampoo from "../public/shampoo_bar.jpg";
 
-const BathSoaps = () => {
+export default function BathSoaps(props) {
   return (
     <>
-      <main>
-        <Heading>Bath Soaps</Heading>
-        <SubHeading>Out of Stock</SubHeading>
+      <main className={styles.main}>
+        <Heading>Bath Soap</Heading>
+        <MainImage>
+          <Image src={shampoo} alt="Shampoo Bar Image" />
+        </MainImage>
+        <p className={styles.blurb}>
+          Like my other soaps, my shampoo bars are made entirely from scratch. I
+          have formulated these bars specifically with the hair and scalp in
+          mind rather than simply designating one of my bath soap formulations
+          as shampoo. Say goodbye to incomprehensible ingredient lists and
+          plastic bottles and hello to natural shampoo that will leave your hair
+          and scalp clean and healthy.
+        </p>
+        <Notifications />
+
+        <ShavingProductsList>
+          {props.inStockBath &&
+            props.inStockBath.length > 0 &&
+            props.inStockBath.map((variety) => (
+              <ShavingProductItem
+                name={variety.scent}
+                price={variety.price}
+                description={variety.description}
+                inStock={variety.inStock}
+                type={variety.type}
+                id={variety._id}
+                weight={variety.weight}
+                key={variety._id}
+              />
+            ))}
+          {props.outOfStockBath &&
+            props.outOfStockBath.length > 0 &&
+            props.outOfStockBath.map((variety) => (
+              <ShavingProductItem
+                name={variety.scent}
+                price={variety.price}
+                description={variety.description}
+                inStock={variety.inStock}
+                type={variety.type}
+                id={variety._id}
+                weight={variety.weight}
+                key={variety._id}
+              />
+            ))}
+        </ShavingProductsList>
       </main>
+      <Footer />
     </>
   );
-};
+}
 
-export default BathSoaps;
+export async function getServerSideProps(context) {
+  const [inStockBath, outOfStockBath] = await orderProductList("bath");
+
+  return {
+    props: {
+      inStockBath: JSON.parse(JSON.stringify(inStockBath)),
+      outOfStockBatch: JSON.parse(JSON.stringify(outOfStockBath)),
+    },
+  };
+}
