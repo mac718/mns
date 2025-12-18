fc36f9cbc230c0ba8b3c2b844393b2d0db913de7
Mike Coon <mac718@gmail.com>
2025-12-18 11:07:12 -0500

    updated Notifications

--- components/Notifications.js
+++ components/Notifications.js
@@ -5,7 +5,7 @@ const Notifications = () => {
     <aside className={styles.notifications}>
       **In response to overwhelming demand, we are reintroducing the Barbershop
       scent, which we had phased out in 2024. We are currently processing orders
-      from 12/11-12/12. Thanks! **
+      from 12/17. Thanks! **
     </aside>
   );
 };
