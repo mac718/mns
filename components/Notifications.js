4e3286eb2a5f08a0d3c6f934daa23bfd482cf01e
Mike Coon <mac718@gmail.com>
2026-08-20 08:59:37 -0400

    updated Notifications

--- components/Notifications.js
+++ components/Notifications.js
@@ -4,7 +4,7 @@ const Notifications = () => {
   return (
     <aside className={styles.notifications}>
       **The Mike&apos;s Natural Soaps Family has relocated to Easton, PA. We are
-      currently processing orders from 8/15-8/17. Thanks! **
+      currently processing orders from 8/19. Thanks! **
     </aside>
   );
 };
